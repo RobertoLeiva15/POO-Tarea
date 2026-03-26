7b1f8fe99019c486407cd29adaed6c46f9feca22
Jose Roberto Leiva Salazar <jleivas@miumg.edu.gt>
2026-03-26 01:39:19 -0600

    Clase Persona

--- src/Encapsulacion/Main.java
+++ src/Encapsulacion/Main.java
@@ -1,9 +1,19 @@
 package Encapsulacion;
 
 public class Main {
-
+	
+	//Clase Persona
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Persona p1=new Persona("Roberto", "Leiva", 19);
+	Persona p2=new Persona("Carmen", "Salazar", 41);
+	Persona p3=new Persona("Mynor", "Leiva", 43);
+	
+	p1.mostrar();
+	p2.mostrar();
+	p3.mostrar();
+	
+	//Clase Rectangulo
+	
 
 	}
 
---
+++ src/Encapsulacion/Persona.java
@@ -0,0 +1,20 @@
+package Encapsulacion;
+
+public class Persona {
+
+	//ATRIBUTOS 
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	public Persona(String nombre, String apellido, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+	public void mostrar() {
+		System.out.println("Nombre: " + nombre + " " + apellido + " Edad: " + edad);
+	}
+	
+}
